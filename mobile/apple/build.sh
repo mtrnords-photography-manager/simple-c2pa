9a9acc9b135e610e169232487a89b9ff6d73d975
Darren Clarke <darren@redaranj.com>
2023-11-09 08:16:49 +0100

    More apple build script updates

--- mobile/apple/build.sh
+++ mobile/apple/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-MOBILE_DIR="mobile"
+MOBILE_DIR="$(pwd)/mobile"
 APPLE_DIR="${MOBILE_DIR}/apple"
 TARGET_DIR="${APPLE_DIR}/target"
 OUT_DIR="${APPLE_DIR}/out"
@@ -28,7 +28,9 @@ mkdir -p "${NEW_HEADER_DIR}"
 cp "${HEADER_PATH}" "${NEW_HEADER_DIR}/"
 cp "${BINDINGS_DIR}/${NAME}FFI.modulemap" "${NEW_HEADER_DIR}/module.modulemap"
 
-rm -rf "${OUTDIR}/${NAME}_framework.xcframework"
+mkdir -p ${OUT_DIR}
+rm -rf "${OUT_DIR}/${NAME}_framework.xcframework"
+
 
 xcrun lipo -create -output "${TARGET_DIR}/simulators.a" "${TARGET_DIR}/aarch64-apple-ios-sim/${BUILD_MODE}/${STATIC_LIB_NAME}" "${TARGET_DIR}/x86_64-apple-ios/${BUILD_MODE}/${STATIC_LIB_NAME}"
 
