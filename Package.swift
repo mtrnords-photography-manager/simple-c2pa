a222d5bcbc5938e108e32864b4db6bc9471691a0
Darren Clarke <darren@redaranj.com>
2023-12-06 10:35:29 +0100

    Add checksum

--- Package.swift
+++ Package.swift
@@ -17,6 +17,7 @@ let package = Package(
         .binaryTarget(
             name: "SimpleC2PAFramework",
              url: "https://gitlab.com/api/v4/projects/51891540/packages/generic/simple_c2pa/apple-v0.174/SimpleC2PA.xcframework.zip",
+             checksum: "c43d3e336ff834e505123b150f5ed57154c558675cb900e5e20055c6a1bd2f11"
         ),
         .target(
             name: "SimpleC2PA",
