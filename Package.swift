f4b2ae6c6bf4aa362fc32f4e5f057168b20ff098
Darren Clarke <darren@redaranj.com>
2023-12-07 07:26:53 +0000

    Update Swift files

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SimpleC2PAFramework",
-             url: "https://gitlab.com/api/v4/projects/51891540/packages/generic/simple_c2pa/apple-v0.174/SimpleC2PA.xcframework.zip",
-             checksum: "c43d3e336ff834e505123b150f5ed57154c558675cb900e5e20055c6a1bd2f11"
+             url: "https://gitlab.com/api/v4/projects/51891540/packages/generic/simple_c2pa/0.0.11/SimpleC2PA.xcframework.zip",
+             checksum: "17e7a1b81abf869a2cdb8ecb336e406b5ee9245ecce9daf6513bbdb22dff5ac7"
         ),
         .target(
             name: "SimpleC2PA",
