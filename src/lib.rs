3ce89fdd55a7f0c4ccbe7601be453317189288fd
MTRNord <mtrnord1@gmail.com>
2024-06-22 11:48:21 +0200

    Add missing imports and use local patched sdk for cr3

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,18 @@
+pub use assertions::{AIDataMiningUsage, CustomAITrainingOptions, ExifData};
+pub use certificates::{
+    Certificate, CertificateOptions, CertificateType, create_certificate,
+    create_content_credentials_certificate, create_private_key, create_root_certificate, request_signed_certificate,
+};
+pub use common::FileData;
+pub use content_credentials::{ApplicationInfo, ContentCredentials};
+
 uniffi::setup_scaffolding!("simple_c2pa");
 
 mod common;
-pub use common::FileData;
 
 mod certificates;
-pub use certificates::{
-    create_certificate, create_content_credentials_certificate, create_private_key,
-    create_root_certificate, request_signed_certificate, CertificateOptions, CertificateType,
-};
 
 mod content_credentials;
-pub use content_credentials::{ApplicationInfo, ContentCredentials};
 
 mod assertions;
-pub use assertions::{AIDataMiningUsage, CustomAITrainingOptions, ExifData};
+
